d94363130201bb6182f4277e9074866ac6ebe739
Eren <eren.gumus@idb.jsb>
2021-11-22 16:00:07 +0300

    MainActivity'de db açıldı. veriler çekildi. RecyclerView adapter oluşturuldu ve MainActivity'de gösterildi.

--- app/src/main/java/com/example/examplerxjava/view/MainActivity.java
+++ app/src/main/java/com/example/examplerxjava/view/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.examplerxjava.view;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,18 +12,48 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.example.examplerxjava.R;
+import com.example.examplerxjava.adapter.PlaceAdapter;
 import com.example.examplerxjava.databinding.ActivityMainBinding;
+import com.example.examplerxjava.model.Place;
+import com.example.examplerxjava.roomdb.PlaceDao;
+import com.example.examplerxjava.roomdb.PlaceDatabase;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
+    private PlaceDatabase db;
+    private PlaceDao placeDao;
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        db = Room.databaseBuilder(getApplicationContext(), PlaceDatabase.class, "Places").build();
+        placeDao = db.placeDao();
+
+        compositeDisposable.add(placeDao.getAll()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(MainActivity.this::handleResponse) //insert ile delete bana bişey döndürmüyordu ama getAll List<Place> döndürüyor. Bu listeyi, veriyi handleResponse metodunda işleyebiirim.
+        );
+
+    }
+
+    private void handleResponse(List<Place> placeList) {
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        PlaceAdapter placeAdapter = new PlaceAdapter(placeList);
+        binding.recyclerView.setAdapter(placeAdapter);
     }
 
     @Override
@@ -39,5 +71,12 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        compositeDisposable.clear();
     }
 }
