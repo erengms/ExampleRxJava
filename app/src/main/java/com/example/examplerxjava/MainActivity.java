6cf0053ba54a9432b1eef816a10afc04461ea6eb
Eren <eren.gumus@idb.jsb>
2021-11-22 13:30:12 +0300

    görünümler ayarlandı

--- app/src/main/java/com/example/examplerxjava/MainActivity.java
+++ app/src/main/java/com/example/examplerxjava/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.examplerxjava;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.examplerxjava.databinding.ActivityMainBinding;
 
@@ -15,5 +20,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.travel_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.add_place) {
+            Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/examplerxjava/model/Place.java
@@ -0,0 +1,5 @@
+package com.example.examplerxjava.model;
+
+public class Place {
+
+}
