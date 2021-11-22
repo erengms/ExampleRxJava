f94dca277abad9fefa5187bce3426a8b583ece6a
Eren <eren.gumus@idb.jsb>
2021-11-22 13:37:30 +0300

    Room database'e giriş. Model sınıfını oluşturduk.

--- app/src/main/java/com/example/examplerxjava/model/Place.java
+++ app/src/main/java/com/example/examplerxjava/model/Place.java
@@ -1,5 +1,28 @@
 package com.example.examplerxjava.model;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Place {
+
+    @PrimaryKey(autoGenerate = true) //otomatik id oluşturur
+    public int id;
+
+    @ColumnInfo(name = "name") //column ismi verdik
+    public String name;
+
+    @ColumnInfo(name = "latitude")
+    public double latitude;
+
+    @ColumnInfo(name = "longitude")
+    public double longitude;
+
+    public Place(String name, double latitude, double longitude){
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 
 }
