d867d5f87861cc0418637bfcf36f9f69f7735f4c
Eren <eren.gumus@idb.jsb>
2021-12-01 16:17:36 +0300

    RxJava ile yapılan Search eklendi.

--- app/src/main/java/com/example/examplerxjava/adapter/PlaceAdapter.java
+++ app/src/main/java/com/example/examplerxjava/adapter/PlaceAdapter.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,14 +14,18 @@ import com.example.examplerxjava.databinding.RecyclerRowBinding;
 import com.example.examplerxjava.model.Place;
 import com.example.examplerxjava.view.MapsActivity;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.PlaceHolder> {
+public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.PlaceHolder>  implements Filterable {
 
     List<Place> placeList;
+    private List<Place> placeListForSearch;
 
     public PlaceAdapter(List<Place> placeList){
         this.placeList = placeList;
+
+        placeListForSearch = new ArrayList<>(placeList);
     }
 
     @NonNull
@@ -48,6 +54,41 @@ public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.PlaceHolder>
     @Override
     public int getItemCount() {
         return placeList.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                List<Place> filteredList = new ArrayList<>();
+
+                if (constraint == null || constraint.length() == 0) {
+                    filteredList.addAll(placeListForSearch);
+                } else {
+                    String filterPattern = constraint.toString().toLowerCase().trim();
+
+                    for (Place item : placeListForSearch) {
+                        if (item.name.toLowerCase().contains(filterPattern)) {
+                            filteredList.add(item);
+                        }
+                    }
+                }
+
+                FilterResults results = new FilterResults();
+                results.values = filteredList;
+
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                placeList.clear();
+                placeList.addAll((List) results.values);
+                notifyDataSetChanged();
+            }
+        };
     }
 
     public class PlaceHolder extends RecyclerView.ViewHolder {
--- app/src/main/java/com/example/examplerxjava/view/MainActivity.java
+++ app/src/main/java/com/example/examplerxjava/view/MainActivity.java
@@ -4,12 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.room.Room;
+import androidx.room.rxjava3.RxRoom;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.SearchView;
 
 import com.example.examplerxjava.R;
 import com.example.examplerxjava.adapter.PlaceAdapter;
@@ -19,9 +22,15 @@ import com.example.examplerxjava.roomdb.PlaceDao;
 import com.example.examplerxjava.roomdb.PlaceDatabase;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableEmitter;
+import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 
@@ -32,6 +41,10 @@ public class MainActivity extends AppCompatActivity {
     private PlaceDatabase db;
     private PlaceDao placeDao;
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+    private PlaceAdapter placeAdapter;
+
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void handleResponse(List<Place> placeList) {
         binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        PlaceAdapter placeAdapter = new PlaceAdapter(placeList);
+        placeAdapter = new PlaceAdapter(placeList);
         binding.recyclerView.setAdapter(placeAdapter);
     }
 
@@ -60,8 +73,14 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.travel_menu, menu);
+
+        MenuItem item = menu.findItem(R.id.search_all);
+        searchView = (SearchView) item.getActionView();
+        search();
+
         return super.onCreateOptionsMenu(menu);
     }
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -72,6 +91,58 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void search() {
+
+        Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(@io.reactivex.rxjava3.annotations.NonNull ObservableEmitter<String> emitter) throws Throwable {
+                searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                    @Override
+                    public boolean onQueryTextSubmit(String query) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onQueryTextChange(String newText) {
+                        if (!emitter.isDisposed()){
+                            emitter.onNext(newText); //Girilen her terimden sonra Observer'ın onNext Metoduna düş
+                        }
+
+                        return false;
+
+                    }
+                });
+            }
+        })
+                .distinctUntilChanged() //Önceden arama yapılan bir terim için tekrar arama yapma
+                .debounce(200, TimeUnit.MILLISECONDS) //Girilen terimden sonra 200ms bekle
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(@io.reactivex.rxjava3.annotations.NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@io.reactivex.rxjava3.annotations.NonNull String s) {
+                        Log.d("@@@@", "Search: " + s);
+                        if (placeAdapter != null) placeAdapter.getFilter().filter(s);
+                    }
+
+                    @Override
+                    public void onError(@io.reactivex.rxjava3.annotations.NonNull Throwable e) {
+                        Log.d("@@@@", "error search: " + e);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d("@@@@", "Complete search: " );
+
+                    }
+                });
     }
 
     @Override
